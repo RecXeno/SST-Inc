40a78f23e887b0cfb5ebc04c9fbad4b295d6d4f6
Rayden Chew <raydencjr@gmail.com>
2024-09-09 16:00:52 +0800

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Chew Jin Rui Rayden on 9/9/24.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
